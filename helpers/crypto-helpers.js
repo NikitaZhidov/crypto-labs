0b9f4a16a803cdae6f25665de92b0d649c6f516f
NikitaZhidov <nzhidov02@gmail.com>
2022-04-24 20:20:40 +0300

    add crypto helpers

--- helpers/crypto-helpers.js
+++ helpers/crypto-helpers.js
@@ -1,59 +1,13 @@
-// src.js
-
 const random = require('random-bigint');
 
-function randomIntFromInterval(min, max) {
-    const int = Math.floor(Math.random() * (max - min + 1) + min)
-    return BigInt(int);
-}
-
-function RSA(N) {
-
-    let p, q
-    do {
-        p = generatePrime(N);
-        q = generatePrime(N);
-    } while (p == q)
-
-    const f = (p - 1n)*(q - 1n);
-    var e = 1n;
-
-    let t, x, y;
-    do {
-        e += 2n;
-        [t, x, y] = extendedEuclid(e, f);
-    } while (t > 1n)
-
-    const n = p * q;
-    const d = (x < 0n ? x + f : x) % f;
-
-    const answer = [e, d, n, p, q];
-    return answer;
-}
-
-function decryptRSA(cmsg, d, p, q, n) {
-    d1 = d % (p - 1n);
-    d2 = d % (q - 1n);
-
-    m1 = modExp(cmsg, d1, p);
-    m2 = modExp(cmsg, d2, q);
-
-    let t, x, y;
-    [t, x, y] = extendedEuclid(q, p);
-
-    r = (x < 0n ? x + p : x) % p;
-    msg = (((m1 - m2) * r) % p) * q + m2;
-    return msg;
-}
-
 function generatePrime(maxBits) {
-    let n;
-    do {
-        n = random(maxBits);
-        n = 2n * n - 1n;
-    } while (!isPrime(n))
+	let n;
+	do {
+		n = random(maxBits);
+		n = 2n * n - 1n;
+	} while (!isPrime(n));
 
-    return n;
+	return n;
 }
 
 function isPrime(n) {
@@ -130,6 +84,12 @@ function modExp(a, b, n) {
     return (a * x) % n;
 }
 
+function randomIntFromInterval(min, max) {
+    const int = Math.floor(Math.random() * (max - min + 1) + min)
+    return BigInt(int);
+}
+
+
 function extendedEuclid(a, b) {
     if (b == 0n)
         return [a, 1n, 0n];
@@ -142,34 +102,11 @@ function extendedEuclid(a, b) {
     return [d, x, y];
 }
 
-function main(message) {
-    const [e, d, n, p ,q] = RSA(100);
-    console.log('Открытый ключ:', e, 'и', n);
-    console.log('Закрытый ключ:', d, 'и', n);
-
-    const signature = modExp(BigInt(message), d, n);
-    console.log('Цифровая подпись:', signature);
-
-    const approval = Number(decryptRSA(signature, e, p, q, n));
-    if (approval == message)
-        console.log(`Все ок, подпись валидна! Сообщение: ${approval}.`);
-    else
-        console.log('Исправляйте алгоритм');
+module.exports = {
+	extendedEuclid,
+	randomIntFromInterval,
+	modExp,
+	rabinMiller,
+	isPrime,
+	generatePrime,
 }
-
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-})
-
-readline.question('Введите сообщение (число): ', (msg) => {
-    if (isNaN(msg)) {
-        console.warn('Введены некорректные данные');
-        process.exit();
-    }
-
-    const msgNum = Number(msg);
-    main(msgNum);
-    process.exit();
-})
-
---
+++ rsa/src.js
@@ -0,0 +1,75 @@
+// src.js
+
+const { generatePrime, modExp, extendedEuclid } = require('../helpers/crypto-helpers');
+
+function RSA(N) {
+
+    let p, q
+    do {
+        p = generatePrime(N);
+        q = generatePrime(N);
+    } while (p == q)
+
+    const f = (p - 1n)*(q - 1n);
+    var e = 1n;
+
+    let t, x, y;
+    do {
+        e += 2n;
+        [t, x, y] = extendedEuclid(e, f);
+    } while (t > 1n)
+
+    const n = p * q;
+    const d = (x < 0n ? x + f : x) % f;
+
+    const answer = [e, d, n, p, q];
+    return answer;
+}
+
+function decryptRSA(cmsg, d, p, q, n) {
+    d1 = d % (p - 1n);
+    d2 = d % (q - 1n);
+
+    m1 = modExp(cmsg, d1, p);
+    m2 = modExp(cmsg, d2, q);
+
+    let t, x, y;
+    [t, x, y] = extendedEuclid(q, p);
+
+    r = (x < 0n ? x + p : x) % p;
+    msg = (((m1 - m2) * r) % p) * q + m2;
+    return msg;
+}
+
+
+function main(message) {
+    const [e, d, n, p ,q] = RSA(100);
+    console.log('Открытый ключ:', e, 'и', n);
+    console.log('Закрытый ключ:', d, 'и', n);
+
+    const signature = modExp(BigInt(message), d, n);
+    console.log('Цифровая подпись:', signature);
+
+    const approval = Number(decryptRSA(signature, e, p, q, n));
+    if (approval == message)
+        console.log(`Все ок, подпись валидна! Сообщение: ${approval}.`);
+    else
+        console.log('Исправляйте алгоритм');
+}
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+readline.question('Введите сообщение (число): ', (msg) => {
+    if (isNaN(msg)) {
+        console.warn('Введены некорректные данные');
+        process.exit();
+    }
+
+    const msgNum = Number(msg);
+    main(msgNum);
+    process.exit();
+})
+
